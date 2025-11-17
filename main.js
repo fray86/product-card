cad2899e121ecfaf8e0e57380c4c1a18d9de92f7
kotof <kotofilde@gmail.com>
2025-11-17 10:57:53 +0500

    fix errors

--- main.js
+++ main.js
@@ -1,61 +1,61 @@
 // Покраска всех карточек
 
-const productCards = document.querySelectorAll('.card-container');
+const productCards = document.querySelectorAll(".card-container");
 const changeProductCardsColorButton = document.querySelector(".change-all-color-card");
-const RedColorHash = '#FF0000';
-const PinkColorHash = '#FFC0CB';
-const urlGoogle = 'http://google.com';
+const redColorHash = "#FF0000";
 
-changeProductCardsColorButton.addEventListener('click', () => {
-    productCards.forEach((card) => card.style.backgroundColor = RedColorHash)
-})
+changeProductCardsColorButton.addEventListener("click", () => {
+  productCards.forEach((card) => (card.style.backgroundColor = redColorHash));
+});
 
 // Покраска одной карточки
 
-const firstProductCard = document.querySelector('.card-container');
-const changeProductCardColorButton = document.querySelector('.change-color-card');
+const firstProductCard = document.querySelector(".card-container");
+const changeProductCardColorButton = document.querySelector(".change-color-card");
+const pinkColorHash = "#FFC0CB";
 
-changeProductCardColorButton.addEventListener('click', () => {
-    firstProductCard.style.backgroundColor = PinkColorHash;
-})
+changeProductCardColorButton.addEventListener("click", () => {
+  firstProductCard.style.backgroundColor = pinkColorHash;
+});
 
 // Кнопка перехода на другую страницу
 
-const openGoogleBtn = document.querySelector('#open-google-page');
+const openGoogleBtn = document.querySelector("#open-google-page");
+const urlGoogle = "http://google.com";
 
-openGoogleBtn.addEventListener('click', openGoogle) 
+openGoogleBtn.addEventListener("click", openGoogle);
 
-function openGoogle () {
-    const answer = confirm('Вы действительно хотите открыть Google?');
-    if (answer === true) {
-        window.open(urlGoogle)
-    } else {
-        return;
-    }
+function openGoogle() {
+  const answer = confirm("Вы действительно хотите открыть Google?");
+  if (answer === true) {
+    window.open(urlGoogle);
+  } else {
+    return;
+  }
 }
 
 // Вывод косоль лог
 
-const outputConsoleBtn = document.querySelector('#output-console-log');
+const outputConsoleBtn = document.querySelector("#output-console-log");
 
-outputConsoleBtn.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+outputConsoleBtn.addEventListener("click", () => outputConsoleLog("ДЗ №4"));
 function outputConsoleLog(message) {
-    alert(message)
-    console.log(message)
+  alert(message);
+  console.log(message);
 }
 
 // Вывод заголовка в консоль
 
-const mainTitle = document.querySelector('.main-title')
+const mainTitle = document.querySelector(".main-title");
 
-mainTitle.addEventListener('mouseenter',  () => {
-    console.log(mainTitle.textContent);
+mainTitle.addEventListener("mouseenter", () => {
+  console.log(mainTitle.textContent);
 });
 
 //Кнопка смены цвета с одного на другой
 
-const switchColorBtn = document.querySelector('#switch-color-btn');
+const switchColorBtn = document.querySelector("#switch-color-btn");
 
-switchColorBtn.addEventListener('click', () => {
-    switchColorBtn.classList.toggle('second');
+switchColorBtn.addEventListener("click", () => {
+  switchColorBtn.classList.toggle("bg_blue");
 });
