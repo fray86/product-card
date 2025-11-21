5f8bba4b0ddfa1f231d38e75b3645e4c3629cb40
kotof <kotofilde@gmail.com>
2025-11-21 09:48:05 +0500

    fix functions name, tabs

--- homework-5.js
+++ homework-5.js
@@ -1,57 +1,56 @@
 // Функция: город и температура воздуха.
 
-function weatherInfo (city, temperature) {
-  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`)
+function showWeatherInfo(city, temperature) {
+  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`);
 }
-weatherInfo('Нефтеюганске', -10);
+weatherInfo("Нефтеюганске", -10);
 
 // Функция проверки скорости звука
 
 const SOUND_SPEED = 343;
 
-function speedTest (speed) {
+function speedTest(speed) {
   if (speed > SOUND_SPEED) {
-    console.log('Сверхзвуковая скорость')
+    console.log("Сверхзвуковая скорость");
   } else if (speed < SOUND_SPEED) {
-    console.log('Дозвуковая скорость')
-   } else {
-      console.log('Скорость звука')
-    }
+    console.log("Дозвуковая скорость");
+  } else {
+    console.log("Скорость звука");
   }
+}
 
-speedTest(342)
+speedTest(342);
 
 // Функция подсчета стоимости товаров
 
-const product = 'Курс Angular'
-const price = 200000
+const product = "Курс Angular";
+const price = 200000;
 
-function checkSolvency (budget) {
+function checkSolvency(budget) {
   const remainingBudget = price - budget;
   if (budget >= price) {
-    console.log(`${product} приобретен. Спасибо за покупку!`)
+    console.log(`${product} приобретен. Спасибо за покупку!`);
   } else {
-      console.log(`Вам не хватает ${remainingBudget}, пополните баланс.`);
-    }
+    console.log(`Вам не хватает ${remainingBudget}, пополните баланс.`);
   }
+}
 
-  checkSolvency(200000)
+checkSolvency(200000);
 
-  // Функция на свое усмотрение
+// Функция на свое усмотрение
 
-  function physique(weight) {
-    if (weight >= 100) {
-      console.log('Ты толстоват ')
-    } else {
-      console.log('Ты худоват')
-    }
+function showPhysique(weight) {
+  if (weight >= 100) {
+    console.log("Ты толстоват ");
+  } else {
+    console.log("Ты худоват");
   }
+}
 
-  physique(150)
+showPhysique(150);
 
-  // Переменные на свое усмотрение
+// Переменные на свое усмотрение
 
-  const hateDota = true
-  let bestGame = 'Micecraft'
-  const currentYear = 2025
-  
+const hateDota = false;
+let bestGame = "Micecraft";
+const currentYear = 2025;
