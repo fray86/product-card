f8a553aedc24ee849b5d6c6668183ec193dac6d2
kotof <kotofilde@gmail.com>
2026-03-12 14:35:15 +0500

    complete homework

---
+++ homework-12/Modal.js
@@ -0,0 +1,45 @@
+export class Modal {
+  constructor(modalId, buttonId, shouldCloseOnOverlay) {
+    this.modal = document.getElementById(modalId);
+    this.overlay = document.getElementById('overlay');
+    if (this.modal) {
+    this.#initOpen(buttonId);
+    this.#initClose(shouldCloseOnOverlay);
+    }
+  }
+
+  open() {
+    this.modal.classList.add('modal-showed');
+    this.overlay.classList.add('overlay-showed');
+  }
+
+  close() {
+    if (this.isOpen()) {
+    this.modal.classList.remove('modal-showed');
+    this.overlay.classList.remove('overlay-showed');
+    }
+  }
+
+  isOpen() {
+    return this.modal.classList.contains('modal-showed');
+  }
+
+  #initOpen(buttonId) {
+    const button = document.getElementById(buttonId);
+    button.addEventListener('click', () => {
+      this.open();
+    })
+  }
+
+  #initClose(shouldCloseOnOverlay) {
+    const closeButton = this.modal.querySelector('#modal-close-button')
+      closeButton.addEventListener('click', () => {
+      this.close();
+    })
+    if (shouldCloseOnOverlay) {
+      this.overlay.addEventListener('click', () => {
+        this.close();
+      })
+    }
+    }
+  }
