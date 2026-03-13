449860e11343c3a9948c9c2b66abd22000be0a32
kotof <kotofilde@gmail.com>
2026-03-13 18:16:40 +0500

    fix v2.0

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -1,41 +1,42 @@
 export class Modal {
   constructor(modalId, buttonId, shouldCloseOnOverlay) {
     this.modal = document.getElementById(modalId);
-    this.overlay = document.getElementById('overlay');
+    this.overlay = document.getElementById("overlay");
     this.shouldCloseOnOverlay = shouldCloseOnOverlay;
-    this.#initOpen(buttonId);
-    this.#initClose();
     this.handleClick = () => this.close();
+    this.#initOpen(buttonId);
   }
 
   open() {
-    this.modal.classList.add('modal-showed');
-    this.overlay.classList.add('overlay-showed');
-    if (this.shouldCloseOnOverlay)
-    this.overlay.addEventListener('click', this.handleClick)
+    this.modal.classList.add("modal-showed");
+    this.overlay.classList.add("overlay-showed");
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.addEventListener("click", this.handleClick);
+    }
+    this.#initClose();
   }
 
   close() {
-    this.modal.classList.remove('modal-showed');
-    this.overlay.classList.remove('overlay-showed');
-    this.overlay.removeEventListener('click', this.handleClick)
+    this.modal.classList.remove("modal-showed");
+    this.overlay.classList.remove("overlay-showed");
+    this.overlay.removeEventListener("click", this.handleClick);
+    const closeButton = this.modal.querySelector("#modal-close-button");
+    closeButton.removeEventListener("click", this.handleClick);
   }
 
   isOpen() {
-    return this.modal.classList.contains('modal-showed');
+    return this.modal.classList.contains("modal-showed");
   }
 
   #initOpen(buttonId) {
     const button = document.getElementById(buttonId);
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
       this.open();
-    })
+    });
   }
 
   #initClose() {
-    const closeButton = this.modal.querySelector('#modal-close-button')
-      closeButton.addEventListener('click', () => {
-      this.close();
-    })
+    const closeButton = this.modal.querySelector("#modal-close-button");
+    closeButton.addEventListener("click", this.handleClick);
   }
 }
